04d3fb2b1f69a89b5c5ac37773de3345f25d41d9
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-12 16:01:42 +0100

    initial commit + ADD comments in js to check if a user prompted word is a palydrome using a function

---
+++ Palindroma/JS/app.js
@@ -0,0 +1,16 @@
+/* ESERCIZIO:
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma:
+la funzione deve ritornare true se la parola è palindroma
+deve ritornare false se la parola non è palindroma
+alla fine stampate in console un messaggio per dire all’utente il risultato del controllo.*/
+
+//Dichiarare variabile per prompt in cui chiediamo parola ad utente
+
+//con ciclo for, passare ogni lettera della parola partendo dalla fine (length-1) e arrivando all'inizio (indice è >=0). 
+//Ciclo deve essere inverso, quindi i--.
+//creare variabile in cui concateno ogni lettera durante il ciclo, creando la parola invertita
+
+//Creare funzione con 1 parola in ingresso:
+//SE la variabile parola invertita è uguale alla parola immessa dall'utente, allora ritorna true
+//ALTRIMENTI ritorna false
