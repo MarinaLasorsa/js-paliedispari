0e2c71cebf09446b7433d8b2fb5068b727bdfe36
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-12 18:38:38 +0100

    ADD: code to check if sum of user prompted number and randomly generated number between 1 and 5 is an even or odd number using functions

--- Pari e Dispari/JS/app.js
+++ Pari e Dispari/JS/app.js
@@ -5,14 +5,51 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
 
-//Dichiarare variabile per prompt in cui chiediamo numero all'utente
+
+//
+// FUNZIONI
+//
 
 //Creare funzione che ritorni un numero a caso da 1 a 5
 
-//Sommare variabile numero utente e variabile numero random
+function numberGenerator () {
+    return Math.floor(Math.random() * 5) + 1;
+}
 
 //Creare funzione isEven con un numero in ingresso:
 //SE numero è divisibile per due, ritorna true
 //ALTRIMENTI ritorna false
 
+function isEven (number) {
+
+    if (number % 2 === 0) {
+        return "la somma è pari. Hai vinto!"
+    } else {
+        return "la somma è dispari. Hai perso!"
+    }
+}
+
+
+//
+// MAIN
+//
+
+//Dichiarare variabile per prompt in cui chiediamo numero all'utente e variabile computer
+
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5")); //number
+
+console.log("il tuo numero: ", userNumber);
+
+let computerNumber = numberGenerator();
+
+console.log("il numero del computer: ", computerNumber);
+
+//Dichiarare variabile che somma i due numeri
+
+let numbersSum = userNumber + computerNumber;
+
+console.log("la loro somma: ", numbersSum);
+
 //stampare funzione applicata a somma
+
+console.log(isEven(numbersSum))
