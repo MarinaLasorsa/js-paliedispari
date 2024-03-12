120966afe1185618c83bb231c3fefd3afd682342
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-12 16:34:48 +0100

    ADD: code to check if a user prompted word is a palindrome using a function

--- Palindroma/JS/app.js
+++ Palindroma/JS/app.js
@@ -7,10 +7,31 @@ alla fine stampate in console un messaggio per dire all’utente il risultato de
 
 //Dichiarare variabile per prompt in cui chiediamo parola ad utente
 
+let userWord = prompt("Inserisci una parola"); //string
+
 //con ciclo for, passare ogni lettera della parola partendo dalla fine (length-1) e arrivando all'inizio (indice è >=0). 
 //Ciclo deve essere inverso, quindi i--.
 //creare variabile in cui concateno ogni lettera durante il ciclo, creando la parola invertita
 
+let invertedWord = " ";
+
+for (i = userWord.length-1; i >= 0; i--) {
+    let letter = userWord[i];
+    invertedWord += letter;
+}
+
+//console.log(invertedWord);
+
 //Creare funzione con 1 parola in ingresso:
 //SE la variabile parola invertita è uguale alla parola immessa dall'utente, allora ritorna true
 //ALTRIMENTI ritorna false
+
+function isPalindrome (word) {
+    if (word === invertedWord) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isPalindrome(userWord))
