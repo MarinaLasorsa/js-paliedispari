f2db63f42c74d8176f8e21ec8dce6daa236eb3d6
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-12 17:36:22 +0100

    FIX: variable invertedWord in Palindroma folder + ADD: Pari e Dispari folder with js comments for checking through function if user prompted number summed with random generated number between 1 and 5 result an even or odd number

--- Palindroma/JS/app.js
+++ Palindroma/JS/app.js
@@ -5,28 +5,27 @@ la funzione deve ritornare true se la parola è palindroma
 deve ritornare false se la parola non è palindroma
 alla fine stampate in console un messaggio per dire all’utente il risultato del controllo.*/
 
-//Dichiarare variabile per prompt in cui chiediamo parola ad utente
 
-let userWord = prompt("Inserisci una parola"); //string
-
-//con ciclo for, passare ogni lettera della parola partendo dalla fine (length-1) e arrivando all'inizio (indice è >=0). 
-//Ciclo deve essere inverso, quindi i--.
-//creare variabile in cui concateno ogni lettera durante il ciclo, creando la parola invertita
-
-let invertedWord = " ";
-
-for (i = userWord.length-1; i >= 0; i--) {
-    let letter = userWord[i];
-    invertedWord += letter;
-}
-
-//console.log(invertedWord);
+//
+// FUNZIONI
+//
 
 //Creare funzione con 1 parola in ingresso:
 //SE la variabile parola invertita è uguale alla parola immessa dall'utente, allora ritorna true
 //ALTRIMENTI ritorna false
 
 function isPalindrome (word) {
+    let invertedWord = "";
+
+    //con ciclo for, passare ogni lettera della parola partendo dalla fine (length-1) e arrivando all'inizio (indice è >=0). 
+    //Ciclo deve essere inverso, quindi i--.
+    //creare variabile in cui concateno ogni lettera durante il ciclo, creando la parola invertita
+
+    for (let i = userWord.length-1; i >= 0; i--) {
+        let letter = userWord[i];
+        invertedWord += letter;
+    }
+    
     if (word === invertedWord) {
         return true
     } else {
@@ -34,4 +33,13 @@ function isPalindrome (word) {
     }
 }
 
-console.log(isPalindrome(userWord))
+//
+// MAIN
+//
+
+//Dichiarare variabile per prompt in cui chiediamo parola ad utente
+
+let userWord = prompt("Inserisci una parola"); //string
+
+
+console.log(isPalindrome(userWord));
---
+++ Pari e Dispari/JS/app.js
@@ -0,0 +1,18 @@
+/*ESERCIZIO:
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.*/
+
+//Dichiarare variabile per prompt in cui chiediamo numero all'utente
+
+//Creare funzione che ritorni un numero a caso da 1 a 5
+
+//Sommare variabile numero utente e variabile numero random
+
+//Creare funzione isEven con un numero in ingresso:
+//SE numero è divisibile per due, ritorna true
+//ALTRIMENTI ritorna false
+
+//stampare funzione applicata a somma
